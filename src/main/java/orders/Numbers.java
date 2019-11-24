cf37521da9891dbb9ccf6f38170de8813b81fe2f
sofflen <nixxesgaming@gmail.com>
2019-11-24 16:41:33 +0200

    Additional commit

---
+++ src/main/java/orders/Numbers.java
@@ -0,0 +1,124 @@
+package orders;
+
+class Numbers {
+
+    String[][] one() {
+        String[][] one = new String[7][1];
+        one[0][0] = "   ##   ";
+        one[1][0] = " ####   ";
+        one[2][0] = "   ##   ";
+        one[3][0] = "   ##   ";
+        one[4][0] = "   ##   ";
+        one[5][0] = "   ##   ";
+        one[6][0] = " ###### ";
+        return one;
+    }
+
+    String[][] two() {
+        String[][] two = new String[7][1];
+        two[0][0] = " #######  ";
+        two[1][0] = "##     ## ";
+        two[2][0] = "       ## ";
+        two[3][0] = " #######  ";
+        two[4][0] = "##        ";
+        two[5][0] = "##        ";
+        two[6][0] = "######### ";
+        return two;
+    }
+
+    String[][] three() {
+        String[][] three = new String[7][1];
+        three[0][0] = " #######  ";
+        three[1][0] = "##     ## ";
+        three[2][0] = "       ## ";
+        three[3][0] = " #######  ";
+        three[4][0] = "       ## ";
+        three[5][0] = "##     ## ";
+        three[6][0] = " #######  ";
+        return three;
+    }
+
+    String[][] four() {
+        String[][] four = new String[7][1];
+        four[0][0] = "##        ";
+        four[1][0] = "##    ##  ";
+        four[2][0] = "##    ##  ";
+        four[3][0] = "##    ##  ";
+        four[4][0] = "######### ";
+        four[5][0] = "      ##  ";
+        four[6][0] = "      ##  ";
+        return four;
+    }
+
+    String[][] five() {
+        String[][] five = new String[7][1];
+        five[0][0] = "######## ";
+        five[1][0] = "##       ";
+        five[2][0] = "##       ";
+        five[3][0] = "#######  ";
+        five[4][0] = "      ## ";
+        five[5][0] = "##    ## ";
+        five[6][0] = " ######  ";
+        return five;
+    }
+
+    String[][] six() {
+        String[][] six = new String[7][1];
+        six[0][0] = " #######  ";
+        six[1][0] = "##     ## ";
+        six[2][0] = "##        ";
+        six[3][0] = "########  ";
+        six[4][0] = "##     ## ";
+        six[5][0] = "##     ## ";
+        six[6][0] = " #######  ";
+        return six;
+    }
+
+    String[][] seven() {
+        String[][] seven = new String[7][1];
+        seven[0][0] = "######## ";
+        seven[1][0] = "##    ## ";
+        seven[2][0] = "    ##   ";
+        seven[3][0] = "   ##    ";
+        seven[4][0] = "  ##     ";
+        seven[5][0] = "  ##     ";
+        seven[6][0] = "  ##     ";
+        return seven;
+    }
+
+    String[][] eight() {
+        String[][] eight = new String[7][1];
+        eight[0][0] = " #######  ";
+        eight[1][0] = "##     ## ";
+        eight[2][0] = "##     ## ";
+        eight[3][0] = " #######  ";
+        eight[4][0] = "##     ## ";
+        eight[5][0] = "##     ## ";
+        eight[6][0] = " #######  ";
+        return eight;
+    }
+
+    String[][] nine() {
+        String[][] nine = new String[7][1];
+        nine[0][0] = " #######  ";
+        nine[1][0] = "##     ## ";
+        nine[2][0] = "##     ## ";
+        nine[3][0] = " ######## ";
+        nine[4][0] = "       ## ";
+        nine[5][0] = "##     ## ";
+        nine[6][0] = " #######  ";
+        return nine;
+    }
+
+    public String[][] zero() {
+        String[][] zero = new String[7][1];
+        zero[0][0] = " #######  ";
+        zero[1][0] = "##     ## ";
+        zero[2][0] = "##     ## ";
+        zero[3][0] = "##     ## ";
+        zero[4][0] = "##     ## ";
+        zero[5][0] = "##     ## ";
+        zero[6][0] = " #######  ";
+        return zero;
+    }
+}
---
+++ src/main/java/orders/Order.java
@@ -0,0 +1,118 @@
+package orders;
+
+import java.util.Scanner;
+
+public class Order {
+
+    public void orderNumber() {
+        Numbers numbers = new Numbers();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter your number: ");
+        int x = sc.nextInt();
+        int[] numbs = Integer.toString(x).chars().map(i -> i - '0').toArray();
+        int length = numbs.length;
+        int ops = 2;
+        String[][] alone = new String[7][1];
+        String[][] together = new String[7][length];
+
+        if (numbs[0] == 0)
+            alone = numbers.zero();
+        else if (numbs[0] == 1)
+            alone = numbers.one();
+        else if (numbs[0] == 2)
+            alone = numbers.two();
+        else if (numbs[0] == 3)
+            alone = numbers.three();
+        else if (numbs[0] == 4)
+            alone = numbers.four();
+        else if (numbs[0] == 5)
+            alone = numbers.five();
+        else if (numbs[0] == 6)
+            alone = numbers.six();
+        else if (numbs[0] == 7)
+            alone = numbers.seven();
+        else if (numbs[0] == 8)
+            alone = numbers.eight();
+        else if (numbs[0] == 9)
+            alone = numbers.nine();
+
+        if (numbs.length == 1)
+            together = alone;
+        else {
+            for (int i = 1; i < numbs.length; i++) {
+                if (numbs[i] == 0) {
+                    together = concat(alone, numbers.zero(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 1) {
+                    together = concat(alone, numbers.one(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 2) {
+                    together = concat(alone, numbers.two(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 3) {
+                    together = concat(alone, numbers.three(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 4) {
+                    together = concat(alone, numbers.four(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 5) {
+                    together = concat(alone, numbers.five(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 6) {
+                    together = concat(alone, numbers.six(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 7) {
+                    together = concat(alone, numbers.seven(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 8) {
+                    together = concat(alone, numbers.eight(), ops);
+                    alone = together;
+                    ops++;
+                }
+                if (numbs[i] == 9) {
+                    together = concat(alone, numbers.nine(), ops);
+                    alone = together;
+                    ops++;
+                }
+            }
+        }
+        numberSout(together);
+    }
+
+    private String[][] concat(String[][] arr1, String[][] arr2, int x) {
+        String[][] concat = new String[7][x];
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                concat[i][j] = arr1[i][j];
+            }
+            for (int j = arr1[0].length; j < x; j++) {
+                concat[i][j] = arr2[i][0];
+            }
+        }
+        return concat;
+    }
+
+    private void numberSout(String[][] arr) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < arr[0].length; j++)
+                System.out.print(arr[i][j]);
+            System.out.println();
+        }
+    }
+}
